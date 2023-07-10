e6f99832078789f364193c604bc5b4b723c51e18
xiewen <1123654054@qq.com>
2023-07-10 15:13:40 +0800

    modify

---
+++ my-webpack-demo1/test.es5.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var a = "let a test.js";
+var b = "test.js ";
+var c = 2;
+console.log(a, b, c);
---
+++ my-webpack-demo1/test.js
@@ -0,0 +1,4 @@
+let a = "let a test.js";
+const b = "test.js ";
+let c = 2;
+console.log(a, b, c);
